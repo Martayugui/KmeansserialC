b0df6fafa6fa97e2cfa060caec6df832b6904a4f
Martayugui <marta.mrr28@gmail.com>
2019-07-21 13:39:20 +0200

    Read txt parameters

---
+++ kmeansproject/src/read_par_and_img.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <time.h>
+#include <string.h>
+
+typedef  struct parameters {
+	int k;
+	float minErr;
+	int maxIter;
+	int verbose;
+}parameters;
+
+void readParAndImg (int rows,int columns,int bands,int pixels,float *image,parameters *par,char *namefile,char *nameimage);
+
+
+
