1a52c3e597c1c599bc4d578628b57b767bac1912
Martayugui <marta.mrr28@gmail.com>
2019-07-21 12:26:54 +0200

    Read parameters from txt file

---
+++ eclipse-workspace/KmeansProject/KmeansserialC/Debug/src/main.d
@@ -0,0 +1,3 @@
+src/main.o: ../src/main.c ../src/read_par_and_img.h
+
+../src/read_par_and_img.h:
---
+++ eclipse-workspace/KmeansProject/KmeansserialC/Debug/src/read_par_and_img.d
@@ -0,0 +1,4 @@
+src/read_par_and_img.o: ../src/read_par_and_img.c \
+ ../src/read_par_and_img.h
+
+../src/read_par_and_img.h:
